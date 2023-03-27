ffdd642fc620f39cd49ee2a31093003fb9ee3871
seongjun.oh <tiger8708@gmail.com>
2023-03-27 22:36:51 +0900

    eslint, prettier 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,26 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    root: true,
+    extends: '@react-native-community',
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    env: {
+        jest: true,
+    },
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            rules: {
+                semi: ['error', 'always'],
+                '@typescript-eslint/no-shadow': ['error'],
+                'no-shadow': 'off',
+                'no-undef': 'off',
+                'prettier/prettier': [
+                    'error',
+                    {
+                        endOfLine: 'auto',
+                    },
+                ],
+            },
+        },
+    ],
 };
